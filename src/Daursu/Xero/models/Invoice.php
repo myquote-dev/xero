426bdc595737ff881de763d01ea6b74c01cc8714
MyQuote <dev@builderscrack.co.nz>
2015-10-05 16:29:59 +1300

    Adding some conveinience methods to the Invoice and LineItem models to allow for quick creation and modification of the models.

--- src/Daursu/Xero/models/Invoice.php
+++ src/Daursu/Xero/models/Invoice.php
@@ -20,4 +20,10 @@ class Invoice extends BaseModel {
 		return $this->request('GET', sprintf('%s/%s', $this->getUrl(), $id), array(), "", "pdf");
 	}
 
+	/**
+	 * Some quick and easy setters
+	 *
+	 */
+	public function taxInclusive()  { $this->LineAmountTypes = 'Inclusive'; }
+	public function taxExclusive() { $this->LineAmountTypes = 'Exclusive'; }
 }
--- src/Daursu/Xero/models/LineItem.php
+++ src/Daursu/Xero/models/LineItem.php
@@ -9,4 +9,13 @@ class LineItem extends BaseModel {
 	 */
 	protected $primary_column = 'LineItemID';
 
+	/**
+	 * Simple factory function to instantiate a new line item
+	 *
+	 */
+	static public function make($description, $amount, $fields = [])
+	{
+		if (!isset($fields['Quantity'])) $fields['Quantity'] = 1.00;
+		return new LineItem(array_merge($fields, ['Description' => $description, 'LineAmount' => number_format(round($amount,2),2)]));
+	}
 }
