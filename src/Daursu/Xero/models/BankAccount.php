c1595f67a5bddfbca35184ff0b7cce9e348e0dd0
MyQuote <dev@builderscrack.co.nz>
2015-10-14 16:38:05 +1300

    Adding BankAccount class / xero model.

---
+++ src/Daursu/Xero/models/BankAccount.php
@@ -0,0 +1,11 @@
+<?php namespace Daursu\Xero\Models;
+
+class BankAccount extends BaseModel {
+
+	/**
+	 * The name of the primary column.
+	 *
+	 * @var string
+	 */
+	protected $primary_column = 'AccountID';
+}
